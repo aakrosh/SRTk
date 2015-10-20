69bd28aaaeba18264f5756e7b57c416774375e39
Aakrosh Ratan <aakrosh@users.noreply.github.com>
2015-10-20 11:29:39 -0400

    updated some scores

--- src/align.sh
+++ src/align.sh
@@ -12,11 +12,11 @@ maxsplits=$8
 lzscores="--match=1,1 --gap=3,1"
 lzparams="--ambiguous=iupac --seed=match11 --xdrop=2 --hspthresh=20 --ydrop=5"
 lzfmt="--format=softsam"
-lzflt="--limitperquery=2011"
+lzflt="--limitperquery=2011 --matchcount=25"
 
   ${lastz} \
   ${reference}[unmask,multi] \
   ${reads}[nameparse=full,subsample=${indx}/${threads}] \
   ${lzscores} ${lzparams} ${lzfmt} ${lzflt} \
-| ${sd}/find_best_split -s 1 -i -x ${maxsplits} ${reference} \
+| ${sd}/find_best_split -s 4 -i -x ${maxsplits} ${reference} \
 > ${tmpdir}/alignments.${indx}.lz
